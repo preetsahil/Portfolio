20ae10d821d5d8565ac024af3fdf41c1db814bf6
Sahil <preetsahil289@gmail.com>
2024-08-11 17:23:41 +0530

    images not getting uploaded:test1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+};
 
 export default nextConfig;
