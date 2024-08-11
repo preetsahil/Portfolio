d1f6c2e2f6f1aac8693b3196bfa1084ca818713c
Sahil <preetsahil289@gmail.com>
2024-08-11 18:03:19 +0530

    navigation not working

--- src/data/navMenus.ts
+++ src/data/navMenus.ts
@@ -7,43 +7,42 @@ import {
   faLaptopCode,
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
-import { getFullPath } from "@/utils/getFullPath";
 
 const navMenus: INavMenuItem[] = [
   {
     id: "about",
     title: "About",
-    path: getFullPath("/#about"),
+    path: "/#about",
     section: "about",
   },
   {
     id: "services",
     title: "Services",
-    path: getFullPath("/#services"),
+    path: "/#services",
     section: "services",
   },
   {
     id: "experiences",
     title: "Experiences",
-    path: getFullPath("/#experiences"),
+    path: "/#experiences",
     section: "experiences",
   },
   {
     id: "skills",
     title: "Skills",
-    path: getFullPath("/#skills"),
+    path: "/#skills",
     section: "skills",
   },
   {
     id: "projects",
     title: "Projects",
-    path: getFullPath("/#projects"),
+    path: "/#projects",
     section: "projects",
   },
   {
     id: "contact",
     title: "Contact",
-    path: getFullPath("/#contact"),
+    path: "/#contact",
     section: "contact",
   },
 ];
@@ -53,32 +52,32 @@ export default navMenus;
 export const menuItems: INavItem[] = [
   {
     name: "About",
-    link: getFullPath("/#about"),
+    link: "/#about",
     icon: faUser,
   },
   {
     name: "Services",
-    link: getFullPath("/#services"),
+    link: "/#services",
     icon: faBriefcase,
   },
   {
     name: "Experiences",
-    link: getFullPath("/#experiences"),
+    link: "/#experiences",
     icon: faTimeline,
   },
   {
     name: "Skills",
-    link: getFullPath("/#skills"),
+    link: "/#skills",
     icon: faAward,
   },
   {
     name: "Projects",
-    link: getFullPath("/#projects"),
+    link: "/#projects",
     icon: faLaptopCode,
   },
   {
     name: "Contact",
-    link: getFullPath("/#contact"),
+    link: "/#contact",
     icon: faEnvelope,
   },
 ];
--- src/utils/getFullPath.ts
+++
@@ -1,7 +0,0 @@
-export function getBasePath(): string {
-  return process.env.NODE_ENV === "production" ? "/Portfolio" : "";
-}
-
-export function getFullPath(path: string): string {
-  return `${getBasePath()}${path}`;
-}
